12fb07e6f2554a66b5190ac405d5b008544c2e7c
PtaZeta <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-04-19 18:39:23 +0200

    Migración singles

---
+++ app/Http/Controllers/SingleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSingleRequest;
+use App\Http\Requests\UpdateSingleRequest;
+use App\Models\Single;
+
+class SingleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSingleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Single $single)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Single $single)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSingleRequest $request, Single $single)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Single $single)
+    {
+        //
+    }
+}
