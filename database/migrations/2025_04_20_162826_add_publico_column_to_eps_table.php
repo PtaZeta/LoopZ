9eedda90dbf2def598a9186d3206f1844aca7d6d
PtaZeta <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-04-20 18:30:37 +0200

    Migraciones publico todos los Modelos

--- database/migrations/2025_04_20_162826_add_publico_column_to_eps_table.php
+++ database/migrations/2025_04_20_162826_add_publico_column_to_eps_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('eps', function (Blueprint $table) {
-            //
+            $table->boolean('publico')->default(false);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('eps', function (Blueprint $table) {
-            //
+            $table->dropColumn('publico');
         });
     }
 };
