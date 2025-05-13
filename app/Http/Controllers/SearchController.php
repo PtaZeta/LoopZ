9eb6913b46aa9e66e203c65438a391d220c4a68a
Manurl03 <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-05-13 12:58:48 +0200

    Buscar playlist por propietario y arreglar imagenes profile/Show

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,47 +11,74 @@ use Inertia\Inertia;
 class SearchController extends Controller
 {
     public function index(Request $request)
-    {
-        $query = $request->input('query', '');
+{
+    $query = $request->input('query', '');
 
-        $canciones = Cancion::with('usuarios')
-            ->where('titulo', 'like', "%{$query}%")
-            ->get();
+    // Buscar canciones por título o por nombre de usuario relacionado
+    $canciones = Cancion::with('usuarios')
+        ->where('titulo', 'like', "%{$query}%")
+        ->orWhereHas('usuarios', function ($q) use ($query) {
+            $q->where('name', 'like', "%{$query}%");
+        })
+        ->get();
 
-        $users = User::where('name', 'like', "%{$query}%")->get();
+    // Buscar usuarios por nombre
+    $users = User::where('name', 'like', "%{$query}%")->get();
 
-        $playlists = Contenedor::with('usuarios')
-            ->where('tipo', 'playlist')
-            ->where('nombre', 'like', "%{$query}%")
-            ->get();
+    // Buscar contenedores por nombre o nombre del usuario relacionado
+    $playlists = Contenedor::with('usuarios')
+        ->where('tipo', 'playlist')
+        ->where(function ($q) use ($query) {
+            $q->where('nombre', 'like', "%{$query}%")
+              ->orWhereHas('usuarios', function ($q2) use ($query) {
+                  $q2->where('name', 'like', "%{$query}%");
+              });
+        })
+        ->get();
 
-        $eps = Contenedor::with('usuarios')
-            ->where('tipo', 'ep')
-            ->where('nombre', 'like', "%{$query}%")
-            ->get();
+    $eps = Contenedor::with('usuarios')
+        ->where('tipo', 'ep')
+        ->where(function ($q) use ($query) {
+            $q->where('nombre', 'like', "%{$query}%")
+              ->orWhereHas('usuarios', function ($q2) use ($query) {
+                  $q2->where('name', 'like', "%{$query}%");
+              });
+        })
+        ->get();
 
-        $singles = Contenedor::with('usuarios')
-            ->where('tipo', 'single')
-            ->where('nombre', 'like', "%{$query}%")
-            ->get();
+    $singles = Contenedor::with('usuarios')
+        ->where('tipo', 'single')
+        ->where(function ($q) use ($query) {
+            $q->where('nombre', 'like', "%{$query}%")
+              ->orWhereHas('usuarios', function ($q2) use ($query) {
+                  $q2->where('name', 'like', "%{$query}%");
+              });
+        })
+        ->get();
 
-        $albumes = Contenedor::with('usuarios')
-            ->where('tipo', 'album')
-            ->where('nombre', 'like', "%{$query}%")
-            ->get();
+    $albumes = Contenedor::with('usuarios')
+        ->where('tipo', 'album')
+        ->where(function ($q) use ($query) {
+            $q->where('nombre', 'like', "%{$query}%")
+              ->orWhereHas('usuarios', function ($q2) use ($query) {
+                  $q2->where('name', 'like', "%{$query}%");
+              });
+        })
+        ->get();
 
-        return Inertia::render('Search/Index', [
-            'searchQuery' => $query,
-            'results' => [
-                'canciones' => $canciones,
-                'users'     => $users,
-                'playlists' => $playlists,
-                'eps'       => $eps,
-                'singles'   => $singles,
-                'albumes'   => $albumes
-            ],
-            'filters' => [],
-        ]);
-    }
+    return Inertia::render('Search/Index', [
+        'searchQuery' => $query,
+        'results' => [
+            'canciones' => $canciones,
+            'users'     => $users,
+            'playlists' => $playlists,
+            'eps'       => $eps,
+            'singles'   => $singles,
+            'albumes'   => $albumes
+        ],
+        'filters' => [],
+    ]);
+}
+
 }
 
