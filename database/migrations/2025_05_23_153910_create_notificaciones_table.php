326cfdf18b6ca21d39974bb53070352d3520070b
PtaZeta <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-05-23 17:43:01 +0200

    Migración notificaciones

--- database/migrations/2025_05_23_153910_create_notificaciones_table.php
+++ database/migrations/2025_05_23_153910_create_notificaciones_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notificacions', function (Blueprint $table) {
+        Schema::create('notificaciones
+        ', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('mensaje');
+            $table->boolean('leido')->default(false);
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
@@ -22,6 +27,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notificacions');
+        Schema::dropIfExists('notificaciones
+        ');
     }
 };
